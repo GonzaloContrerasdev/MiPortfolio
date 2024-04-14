48d478d648e3b2535caaef8870fb72e68ce6fb18
Gonzalo Contreras <gonzalocontreras004@gmail.com>
2024-04-13 21:05:17 -0300

    cositas

--- scripts/index.js
+++ scripts/index.js
@@ -1,116 +1,17 @@
-// index.js
-particlesJS("particles-js", {
-    "particles": {
-        "number": {
-            "value": 80,
-            "density": {
-                "enable": true,
-                "value_area": 800
-            }
-        },
-        "color": {
-            "value": "#ffffff"
-        },
-        "shape": {
-            "type": "circle",
-            "stroke": {
-                "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 5
-            },
-            "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100,
-            }
-        },
-        "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-                "enable": true,
-                "speed": 1,
-                "opacity_min": 0.1,
-                "sync": false
-            }
-        },
-        "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-                "enable": false,
-                "speed": 40,
-                "size_min": 0.1,
-                "sync": false
-            }
-        },
-        "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": 6,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-            }
-        }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "repulse"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            },
-            "resize": false
-        },
-        "modes": {
-            "grab": {
-                "distance": 400,
-                "line_linked": {
-                    "opacity": 1
-                }
-            },
-            "bubble": {
-                "distance": 400,
-                "size": 40,
-                "duration": 2,
-                "opacity": 8,
-                "speed": 3
-            },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
-            },
-            "push": {
-                "particles_nb": 4
-            },
-            "remove": {
-                "particles_nb": 2
-            }
-        }
-    },
-    "retina_detect": true
-});
+document.getElementById("checkbox").addEventListener("click", function() {
+    // Obtener el color actual del fondo
+    var currentColor = document.body.style.backgroundColor;
+    // Si el color actual es claro, cambiar a oscuro; de lo contrario, cambiar a claro
+    if (currentColor === "rgb(240, 240, 240)" || currentColor === "#f0f0f0") {
+      document.body.style.backgroundColor = "#333"; // Cambiar a fondo oscuro
+      document.body.style.color = "#f0f0f0";
+    } else {
+      document.body.style.backgroundColor = "#f0f0f0"; // Cambiar a fondo claro
+      document.body.style.color = "#333";
+    }
+  });
 
-var boton = document.getElementById("btn");
+  var boton = document.getElementById("github");
 
 // Agregar un event listener para escuchar el clic en el botón
 boton.addEventListener("click", function() {
